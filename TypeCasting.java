a7e92ad2550988cc93abd6489f5e1813e1229cd1
Aditya rana <adityarana1140@gmail.com>
2025-08-04 00:20:19 +0530

    TypeCasting

---
+++ TypeCasting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args){
+        
+
+        int num  = (int)6.34; // we get the output as 6 because we are typecasting the double value to int
+        System.out.println("The value of num is: " + num); // Output: The value of num is: 6
+
+
+    }
+}
