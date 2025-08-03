f34e99fd4fe94048055a032701beb3f0cc6cab93
Aditya rana <adityarana1140@gmail.com>
2025-08-03 19:31:53 +0530

    Stringargumentstructure

---
+++ stringArgumentStructure.java
@@ -0,0 +1,7 @@
+public class stringArgumentStructure {
+    public static void main(String[] args){
+        System.out.print(args[0]);  // <---- this is the argument structure ismeme main function me dikahta hai ki string array ki form me hai so then we use to show the arguments on the given string .
+        
+
+    }
+}
