6a9eec638133c35859c04452ddcd5b173c5e8948
Aditya rana <adityarana1140@gmail.com>
2025-08-15 10:14:07 +0530

    Java 21 feature

---
+++ StringTemplates.java
@@ -0,0 +1,10 @@
+
+import static java.lang.StringTemplates.Processor.STR;
+
+public class StringTemplates {
+    public static void main(String[] args) {
+        int a =45;
+        int b = 32;
+        System.out.println(STR."value of a is {a} and b is {b}");
+    }
+}
