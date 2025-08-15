bb9198c0c11592296af28bbe551c0e777bd36a29
Aditya rana <adityarana1140@gmail.com>
2025-08-15 10:36:47 +0530

    java 21 features

--- unnamedpatternandVariable.java
+++ unnamedpatternandVariable.java
@@ -3,7 +3,12 @@ class unnamedpatternandVariable {
         try{
             double d = 100/0;
             System.out.println("Value of d is: " + d);
-        }catch(Exception _){
+        }catch(Exception _){ // Using an unnamed pattern variable
+            // The underscore (_) is used as a placeholder for the exception variable
+            // This allows us to catch the exception without naming it
+            // It is a common convention in Java to use an underscore for unnamed variables
+            // This is useful when you want to catch an exception but do not need to use the
+            // exception object itself in the catch block
             // This is an unnamed pattern variable
             // It can be used to catch any exception without naming it
             System.out.println("Error");
